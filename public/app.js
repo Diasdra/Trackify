199781e64d1aaa1fc75cb00e46e9ab2ad8ae6fd2
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2021-05-05 16:15:27 -0300

    Fix search functionality

--- public/app.js
+++ public/app.js
@@ -2,10 +2,13 @@ const filter = document.querySelector("#filter")
 const tableBody = document.querySelector(".table-body")
 
 filter.addEventListener("keyup", e => {
-  table = tableBody.querySelectorAll("tr")
+  let filterValue = filter.value.toLowerCase()
+  let table = tableBody.querySelectorAll("tr")
+
   for (let product of table) {
-      if (!(product.item(0).innerHTML.includes(filter.value.toLowerCase()))) {
-          product.style.display = "none"
+      let currentItem = product.cells.item(0).innerHTML.toLowerCase()
+      if (!(currentItem.includes(filterValue) )) {
+        product.style.display = "none"
       } else {
           product.style.display = ""
       }
