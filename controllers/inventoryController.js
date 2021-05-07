04ab3a087430fd66fba9eb527c539b77c0411f53
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2021-05-06 23:25:30 -0300

    Add tests for create route

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,17 +9,27 @@ let inventoryController = {
   },
 
   create: async (req, res) => {
-    await inventoryModel.addInventory({
-      product: req.body.product,
-      product_category: req.body.product_category,
-      qty: parseInt(req.body.qty),
-      price: parseFloat(req.body.price),
-      vendor: req.body.vendor,
-      location: req.body.location
-    })
-    .then(() => {
-      res.redirect("/inventory");
-    })
+    // console.log(
+    //   req.body.qty, req.body.price, req.body.product, req.body.product_category, 
+    //   req.body.vendor, req.body.location)
+    const qty = Number(req.body.qty)
+    const price = Number(req.body.price)
+    if(isNaN(qty) || isNaN(price)) {
+      console.log('Error in data')
+      res.status(400).send({"error": "Wrong data entered"})
+    } else {
+      await inventoryModel.addInventory({
+        product: req.body.product,
+        product_category: req.body.product_category,
+        qty: parseInt(req.body.qty),
+        price: price,
+        vendor: req.body.vendor,
+        location: req.body.location
+      })
+      .then(() => {
+        res.redirect("/inventory");
+      })
+    }
   },
 
   update: async (req, res) => {
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const app = express();
 const path = require("path");
 const ejsLayouts = require("express-ejs-layouts");
+
+const express = require("express");
+const app = express();
 const session = require("express-session")
 const inventoryController = require("./controllers/inventoryController");
+
 const PrismaClient = require("@prisma/client").PrismaClient
 const prisma = new PrismaClient()
-
 
 require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(ejsLayouts);
 app.use(session({
   key: 'user_sid',
@@ -24,14 +26,14 @@ app.use(session({
 }));
 
 //Debugging console logs
-app.use(async (req, res, next) => {
-  console.log("Inventory Database is");
-  req.inventory = await prisma.inventory.findMany()
-  console.log(req.inventory)
-  console.log("Entire session object:");
-  console.log(req.session);
-  next();
-});
+// app.use(async (req, res, next) => {
+//   console.log("Inventory Database is");
+//   req.inventory = await prisma.inventory.findMany()
+//   console.log(req.inventory)
+//   console.log("Entire session object:");
+//   console.log(req.session);
+//   next();
+// });
 
 
 app.set("views", path.join(__dirname, "/views"))
@@ -46,8 +48,12 @@ app.post('/create', inventoryController.create)
 app.get('/inventory', inventoryController.list)
 
 
-app.listen(8000, function () {
-  console.log(
-    "Server running. Visit: http://localhost:8000 in your browser 🚀"
-  );
-});
+run = app.listen(8000, function () {
+        console.log(
+          "Server running. Visit: http://localhost:8000 in your browser 🚀"
+        );
+      });
+
+module.exports.app = app
+module.exports.run = run
+module.exports.prisma = prisma
