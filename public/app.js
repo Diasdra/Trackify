f1bfd7ce99a18d48c81d82ac66e8cc5b54332011
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2021-05-12 22:44:52 -0300

    Add edit button and dynamic input field

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const filter = document.querySelector("#filter")
 console.log(filter)
 const tableBody = document.querySelector(".table-body")
+let editBtn = document.querySelectorAll(".edit-btn")
 
 filter.addEventListener("keyup", e => {
   let filterValue = filter.value.toLowerCase()
@@ -18,3 +19,53 @@ filter.addEventListener("keyup", e => {
       
   }
 })
+
+let editing = false
+let currentEditing = 0
+for (let btn of editBtn) {
+  btn.addEventListener("click", (e) => {
+    let row = e.target.parentNode.parentNode
+    console.log(editing)
+    // Checks if user is already editing an item
+    if (!(editing)) {
+      editing = true
+      // Iterate through the cells in the row
+      for (let cell = 0; cell < 8; cell++) {
+        // If cell is the productID, ignore
+        if (cell === 2) {
+          currentEditing = row.cells[cell].innerHTML
+        // If cell is button
+        } else if (cell === 7) {
+          row.cells[cell].querySelector('.edit-btn').innerHTML = 'Save'
+          // Else edit the cell
+        } else {
+          let cellValue = row.cells[cell].innerHTML
+          row.cells[cell].innerHTML = `<input type="text" value="${cellValue}">`
+        }
+      }
+    } else {
+      editing = false
+      // Iterate through cells in the row
+      for (let cell = 0; cell < 8; cell++) {
+        if (cell === 2) {
+          console.log("hi")
+        } else if (cell === 7) {
+          row.cells[cell].querySelector('.edit-btn').innerHTML = 'Edit'
+        } else {
+          let cellValue = row.cells[cell].querySelector('input').value
+          console.log(cellValue)
+          row.cells[cell].innerHTML = cellValue
+        }
+      }
+    }
+  })
+}
+
+// <td><input type="text" name="<%=item.id%>_product" value="<%=item.product %>"></td>
+// <td><input type="text" name="<%=item.id%>_product_category" value="<%=item.product_category %>"></td>
+// <td><%=item.id %> </td>
+// <td><input type="text" name="<%=item.id%>_qty" value="<%=item.qty %>"></td>
+// <td><input type="text" name="<%=item.id%>_price" value="<%=item.price %>"></td>
+// <td><input type="text" name="<%=item.id%>_vendor" value="<%-item.vendor %>"> </td>
+// <td><input type="text" name="<%=item.id%>_location" value="<%=item.location %>"> </td>
+// <td><button class="edit-btn">Edit</button></td>
